209edde67c7dc7006e0eb75d17fb0ed9d6bf827c
Michael West <mpwest929+github@gmail.com>
2022-10-16 16:35:00 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"os"
+	"log"
+	"fmt"
+	"bytes"
+)
+
+const EOF_MARKER = 0x454f46
+
+func readIpsFile(ipsFile string) {
+	startingBytes := []byte{0x50, 0x41, 0x54, 0x43, 0x48}
+	f, err := os.Open(ipsFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	header := make([]byte, 5)
+	n1, err := f.Read(header)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%d bytes: %s\n", n1, string(header[:n1]))
+
+	res := bytes.Compare(header, startingBytes)
+	if (res != 0) {
+		log.Fatal("The header bytes are incorrect")
+	}
+
+	f.Close();
+}
+
+func main() {
+	argsWithoutProg := os.Args[1:]
+	ipsFile := argsWithoutProg[0]
+	baseRomFile := argsWithoutProg[1]
+	fmt.Printf("%s rom file\n", baseRomFile)
+	readIpsFile(ipsFile) // "./data/smldx.ips")
+}
