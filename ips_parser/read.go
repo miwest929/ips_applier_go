8ef07c84dc1536c8f6be7322947ae0e99564b779
Michael West <mpwest929+github@gmail.com>
2022-10-18 08:31:15 -0400

    Fix byte alignment issues. Organize bytes into struct chunks

--- ips_parser/read.go
+++ ips_parser/read.go
@@ -9,32 +9,31 @@ import (
 )
 
 type NormalChunk struct {
-	offset        uint64
-	dataLength    uint64
-	data          uint64
+	offset        uint32
+	dataLength    uint32
+	data          []byte
 }
 
-type RunLengthEncodingChunk struct {
-	offset           uint64
-	valueRepeatCount uint64
-	value            uint64
-}
+type RunLengthEncodingChunk struct { offset, valueRepeatCount, value uint32 }
 
-func getNextBytes(f *os.File, byteCount uint64) []byte {
-	chunk := make([]byte, byteCount)
-	bytesRead, err := f.Read(chunk)
+func getNextBytes(f *os.File, byteCount int) []byte {
+	bytes := make([]byte, byteCount)
+	bytesRead, err := f.Read(bytes)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%d bytes: [% x]\n", bytesRead, chunk[:bytesRead])
-	return chunk
+	if bytesRead != byteCount {
+		log.Fatal(err)
+	}
+
+	return bytes
 }
 
 func isRunLengthEncoded(nextBytes []byte) bool {
 	rleIndicator := []byte{0x00, 0x00}
 	isRle := bytes.Compare(nextBytes[3:5], rleIndicator)
-	if (isRle != 0) {
+	if (isRle == 0) {
 		return true;
 	}
 
@@ -44,25 +43,34 @@ func isRunLengthEncoded(nextBytes []byte) bool {
 func isEofMarker(nextBytes []byte) bool {
 	eofMarker := []byte{0x45, 0x4f, 0x46};
 	isEof := bytes.Compare(nextBytes[0:3], eofMarker)
-	if (isEof != 0) {
+	if (isEof == 0) {
+		fmt.Printf("EOF MARKER bytes=[% x]\n", nextBytes[0:3])
 		return true
 	}
 
 	return false
 }
 
-func convertByteArrayToUint64(bytes []byte) uint64 {
-	return binary.BigEndian.Uint64(bytes)
+func convertByteArrayToUint32(bytes []byte) uint32 {
+	if len(bytes) < 4 {
+		// Data is in BigEndian. Need to align the given bytes
+		// so they are 4 bytes long.
+		additional := 4 - len(bytes)
+		for i := 0; i < additional; i++ {
+			// This is method of prepending that I came up with
+			bytes = append( []byte{0x00}, bytes...)
+		}
+	}
+	return binary.BigEndian.Uint32(bytes)
 }
 
 func createNormalChunk(nextBytes []byte) *NormalChunk {
-	normalChunk := NormalChunk{offset: convertByteArrayToUint64(nextBytes[0:3]), dataLength: convertByteArrayToUint64(nextBytes[3:5])}
-	// normalChunk.data = <next-data-Length>
+	normalChunk := NormalChunk{offset: convertByteArrayToUint32(nextBytes[0:3]), dataLength: convertByteArrayToUint32(nextBytes[3:5])}
 	return &normalChunk
 }
 
 func createRunLengthEncodingChunk(nextBytes []byte) *RunLengthEncodingChunk {
-	rleChunk := RunLengthEncodingChunk{offset: convertByteArrayToUint64(nextBytes[0:3])}
+	rleChunk := RunLengthEncodingChunk{offset: convertByteArrayToUint32(nextBytes[0:3])}
 	return &rleChunk
 }
 
@@ -90,22 +98,24 @@ func ReadIpsFile(ipsFile string) {
 	bytes := getNextBytes(f, 5)
 	for !isEofMarker(bytes) {
 		if (isRunLengthEncoded(bytes)) {
-			createRunLengthEncodingChunk := createRunLengthEncodingChunk(bytes)
+			rleChunk := createRunLengthEncodingChunk(bytes)
 
 			bytes := getNextBytes(f, 2)
-			// count := binary.BigEndian.Uint64(repeatedValueBytes)
-			createRunLengthEncodingChunk.valueRepeatCount = convertByteArrayToUint64(bytes)
+			rleChunk.valueRepeatCount = convertByteArrayToUint32(bytes)
 
 			bytes = getNextBytes(f, 1)
-			createRunLengthEncodingChunk.value = convertByteArrayToUint64(bytes)
+			rleChunk.value = convertByteArrayToUint32(bytes)
+
+			fmt.Printf("RunLengthEncodedChunk { offset=%d valueRepeatCount=%d value=%d}\n", rleChunk.offset, rleChunk.valueRepeatCount, rleChunk.value)
 		} else {
 			normalChunk := createNormalChunk(bytes)
-			// convertByteArrayToUint64
-			byte = getNextBytes(f, normalChunk.dataLength)
+			bytes = getNextBytes(f, int(normalChunk.dataLength))
+			normalChunk.data = bytes
 
+			fmt.Printf("NormalChunk { offset=%d dataLength=%d data=[% x]}\n", normalChunk.offset, normalChunk.dataLength, normalChunk.data)
 		}
 
-		
+		bytes = getNextBytes(f, 5)
 	}
 
 	f.Close();
