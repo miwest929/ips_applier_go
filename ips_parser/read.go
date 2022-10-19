649031f8ccb73cdb6a4bf575cb037d0eb20eb7ba
Michael West <mpwest929+github@gmail.com>
2022-10-19 10:01:08 -0400

    Create function for applying the ips chunks

--- ips_parser/read.go
+++ ips_parser/read.go
@@ -1,20 +1,20 @@
 package ips_parser
 
 import (
-	"os"
-	"log"
-	"fmt"
 	"bytes"
 	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
 )
 
 type NormalChunk struct {
-	offset        uint32
-	dataLength    uint32
-	data          []byte
+	Offset     uint32
+	DataLength uint32
+	Data       []byte
 }
 
-type RunLengthEncodingChunk struct { offset, valueRepeatCount, value uint32 }
+type RunLengthEncodingChunk struct{ Offset, ValueRepeatCount, Value uint32 }
 
 func getNextBytes(f *os.File, byteCount int) []byte {
 	bytes := make([]byte, byteCount)
@@ -29,17 +29,17 @@ func getNextBytes(f *os.File, byteCount int) []byte {
 func isRunLengthEncoded(nextBytes []byte) bool {
 	rleIndicator := []byte{0x00, 0x00}
 	isRle := bytes.Compare(nextBytes[3:5], rleIndicator)
-	if (isRle == 0) {
-		return true;
+	if isRle == 0 {
+		return true
 	}
 
-	return false;
+	return false
 }
 
 func isEofMarker(nextBytes []byte) bool {
-	eofMarker := []byte{0x45, 0x4f, 0x46};
+	eofMarker := []byte{0x45, 0x4f, 0x46}
 	isEof := bytes.Compare(nextBytes[0:3], eofMarker)
-	if (isEof == 0) {
+	if isEof == 0 {
 		fmt.Printf("EOF MARKER bytes=[% x]\n", nextBytes[0:3])
 		return true
 	}
@@ -54,19 +54,19 @@ func convertByteArrayToUint32(bytes []byte) uint32 {
 		additional := 4 - len(bytes)
 		for i := 0; i < additional; i++ {
 			// This is method of prepending that I came up with
-			bytes = append( []byte{0x00}, bytes...)
+			bytes = append([]byte{0x00}, bytes...)
 		}
 	}
 	return binary.BigEndian.Uint32(bytes)
 }
 
 func createNormalChunk(nextBytes []byte) *NormalChunk {
-	normalChunk := NormalChunk{offset: convertByteArrayToUint32(nextBytes[0:3]), dataLength: convertByteArrayToUint32(nextBytes[3:5])}
+	normalChunk := NormalChunk{Offset: convertByteArrayToUint32(nextBytes[0:3]), DataLength: convertByteArrayToUint32(nextBytes[3:5])}
 	return &normalChunk
 }
 
 func createRunLengthEncodingChunk(nextBytes []byte) *RunLengthEncodingChunk {
-	rleChunk := RunLengthEncodingChunk{offset: convertByteArrayToUint32(nextBytes[0:3])}
+	rleChunk := RunLengthEncodingChunk{Offset: convertByteArrayToUint32(nextBytes[0:3])}
 	return &rleChunk
 }
 
@@ -78,6 +78,7 @@ func ReadIpsFile(ipsFile string) []interface{} {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer f.Close()
 
 	header := make([]byte, 5)
 	n1, err := f.Read(header)
@@ -88,36 +89,34 @@ func ReadIpsFile(ipsFile string) []interface{} {
 	fmt.Printf("%d bytes: %s\n", n1, string(header[:n1]))
 
 	res := bytes.Compare(header, startingBytes)
-	if (res != 0) {
+	if res != 0 {
 		log.Fatal("The header bytes are incorrect")
 	}
 
 	bytes := getNextBytes(f, 5)
 	for !isEofMarker(bytes) {
-		if (isRunLengthEncoded(bytes)) {
+		if isRunLengthEncoded(bytes) {
 			rleChunk := createRunLengthEncodingChunk(bytes)
 
 			bytes := getNextBytes(f, 2)
-			rleChunk.valueRepeatCount = convertByteArrayToUint32(bytes)
+			rleChunk.ValueRepeatCount = convertByteArrayToUint32(bytes)
 
 			bytes = getNextBytes(f, 1)
-			rleChunk.value = convertByteArrayToUint32(bytes)
+			rleChunk.Value = convertByteArrayToUint32(bytes)
 
 			chunks = append(chunks, rleChunk)
-			fmt.Printf("RunLengthEncodedChunk { offset=%d valueRepeatCount=%d value=%d}\n", rleChunk.offset, rleChunk.valueRepeatCount, rleChunk.value)
+			// fmt.Printf("RunLengthEncodedChunk { offset=%d valueRepeatCount=%d value=%d}\n", rleChunk.Offset, rleChunk.ValueRepeatCount, rleChunk.Value)
 		} else {
 			normalChunk := createNormalChunk(bytes)
-			bytes = getNextBytes(f, int(normalChunk.dataLength))
-			normalChunk.data = bytes
+			bytes = getNextBytes(f, int(normalChunk.DataLength))
+			normalChunk.Data = bytes
 
 			chunks = append(chunks, normalChunk)
-			fmt.Printf("NormalChunk { offset=%d dataLength=%d data=[% x]}\n", normalChunk.offset, normalChunk.dataLength, normalChunk.data)
+			// fmt.Printf("NormalChunk { offset=%d dataLength=%d data=[% x]}\n", normalChunk.Offset, normalChunk.DataLength, normalChunk.Data)
 		}
 
 		bytes = getNextBytes(f, 5)
 	}
-
-	f.Close();
 
 	return chunks
 }
--- main.go
+++ main.go
@@ -1,10 +1,37 @@
 package main
 
 import (
-	"os"
-	"fmt"
 	"applyIpsPatch/ips_parser"
+	"fmt"
+	"log"
+	"os"
 )
+
+func applyIpsPatch(baseFilename string, chunks []interface{}) {
+	baseFile, err := os.Open(baseFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer baseFile.Close()
+
+	for _, ch := range chunks {
+		switch c := ch.(type) {
+		case *ips_parser.NormalChunk:
+			fmt.Printf("NormalChunk: offset=%d\n", c.Offset)
+			_, err := baseFile.Seek(int64(c.Offset), 0)
+			if err != nil {
+				log.Fatal(err)
+			}
+			// baseFile.Write(c.Data)
+			// ytesWritten, err := file.Write(byteSlice)
+
+		case *ips_parser.RunLengthEncodingChunk:
+			fmt.Printf("RunLengthEncodingChunk: offset=%d\n", c.Offset)
+		default:
+			fmt.Printf("Encountered unknown chunk type: %T\n", c)
+		}
+	}
+}
 
 func main() {
 	argsWithoutProg := os.Args[1:]
@@ -12,5 +39,6 @@ func main() {
 	baseRomFile := argsWithoutProg[1]
 	fmt.Printf("%s rom file\n", baseRomFile)
 	chunks := ips_parser.ReadIpsFile(ipsFile)
-	fmt.Printf("Number of chunks %d", len(chunks))
+	fmt.Printf("Number of chunks %d\n\n", len(chunks))
+	applyIpsPatch(baseRomFile, chunks)
 }
